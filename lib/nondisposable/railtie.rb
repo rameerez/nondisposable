44b0cf84a01866f5e7e44a5bf1ed5066f7322d1c
Javi R <4920956+rameerez@users.noreply.github.com>
2024-10-31 03:23:26 +0000

    Fix railtie not importing the right module

--- lib/nondisposable/railtie.rb
+++ lib/nondisposable/railtie.rb
@@ -2,7 +2,7 @@ module Nondisposable
   class Railtie < Rails::Railtie
     initializer 'nondisposable.add_validator' do
       ActiveSupport.on_load(:active_record) do
-        include Nondisposable::Validations
+        include ActiveModel::Validations::NondisposableValidator
       end
     end
   end
