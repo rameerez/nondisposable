38ed753f043b450b0a966e50b673aa3e1add950d
Javi R <4920956+rameerez@users.noreply.github.com>
2024-10-31 02:39:30 +0000

    Migrate wrong migration files into proper migration with ERB to choose the `ActiveRecord::Migration` version and auto id type identification

--- lib/generators/nondisposable/templates/create_nondisposable_disposable_domains.rb
+++
@@ -1,9 +0,0 @@
-class CreateNondisposableDisposableDomains < ActiveRecord::Migration[7.0]
-  def change
-    create_table :nondisposable_disposable_domains do |t|
-      t.string :name, null: false, index: { unique: true }
-
-      t.timestamps
-    end
-  end
-end
