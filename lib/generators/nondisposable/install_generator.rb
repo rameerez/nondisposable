f0b5b04da47af5c394e35c8089778e86e23b4261
Javi R <4920956+rameerez@users.noreply.github.com>
2024-10-31 02:47:50 +0000

    Update install generator with new changes (good migration, no rake task, job to update domains, improved post-install instructions)

--- lib/generators/nondisposable/install_generator.rb
+++ lib/generators/nondisposable/install_generator.rb
@@ -1,47 +1,45 @@
 # frozen_string_literal: true
 
 require 'rails/generators/base'
+require 'rails/generators/active_record'
 
 module Nondisposable
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      include Rails::Generators::Migration
+      include ActiveRecord::Generators::Migration
 
       source_root File.expand_path('templates', __dir__)
 
-      def self.next_migration_number(dirname)
-        next_migration_number = current_migration_number(dirname) + 1
-        ActiveRecord::Migration.next_migration_number(next_migration_number)
+      def self.next_migration_number(dir)
+        ActiveRecord::Generators::Base.next_migration_number(dir)
       end
 
       def create_migration_file
-        migration_template 'create_nondisposable_disposable_domains.rb', 'db/migrate/create_nondisposable_disposable_domains.rb'
+        migration_template 'create_nondisposable_disposable_domains.rb.erb', File.join(db_migrate_path, "create_nondisposable_disposable_domains.rb")
       end
 
       def create_initializer
         template 'nondisposable.rb', 'config/initializers/nondisposable.rb'
       end
 
-      def setup_whenever
-        if File.exist?('config/schedule.rb')
-          append_to_file 'config/schedule.rb' do
-            "\n# Update disposable email domains list daily\nevery 1.day, at: \"4:30 am\" do\n  rake \"nondisposable:update_disposable_domains\"\nend\n"
-          end
-        else
-          create_file 'config/schedule.rb' do
-            "# Use this file to easily define all of your cron jobs.\n#\n# Learn more: http://github.com/javan/whenever\n\n# Update disposable email domains list daily\nevery 1.day, at: \"4:30 am\" do\n  rake \"nondisposable:update_disposable_domains\"\nend\n"
-          end
-        end
+      def create_database_refresh_job
+        template 'disposable_email_domain_list_update_job.rb', 'app/jobs/disposable_email_domain_list_update_job.rb'
       end
 
       def display_post_install_message
         say "\tThe `nondisposable` gem has been successfully installed!", :green
         say "\nTo complete the setup:"
         say "  1. Run 'rails db:migrate' to create the necessary tables."
-        say "  2. Run 'rake nondisposable:update_disposable_domains' to populate the initial list of disposable domains."
-        say "  3. Add 'validates :email, nondisposable: true' to your User model (or any model with an email field)."
-        say "  4. Make sure you have a functional `whenever` gem install that can run cron jobs properly so the disposable emails list is updated daily."
+        say "  2. Run 'Nondisposable::DomainListUpdater.update' to populate the initial list of disposable domains."
+        say "  3. Add 'validates_nondisposable_email_of :email' to your User model (or any model with an email field)."
+        say "  4. Configure your recurrent job according to the README, and make sure you have a functional queuing system (like solid_queue) that can run jobs properly so the disposable emails list is updated regularly."
         say "\nEnjoy your new `nondisposable` users!", :green
+      end
+
+      private
+
+      def migration_version
+        "[#{ActiveRecord::VERSION::STRING.to_f}]"
       end
 
     end
